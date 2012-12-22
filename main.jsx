4514978f45243460b96a60571b559b22e4a18e07
abicky <takeshi.arabiki@gmail.com>
2012-12-22 23:12:40 +0900

    add _Main.saveJSX

--- main.jsx
+++ main.jsx
@@ -4,6 +4,7 @@ import "Human.jsx";
 
 class _Main {
     static function main(args : string[]) : void {
+        _Main.saveJSX("main");
         dom.id("programmer").addEventListener("click", function(e : Event) : void {
             _Main.loadClass("Programmer", function(factory : function(:Map.<variant>) : Human) : void {
                 var abicky = factory({
@@ -28,6 +29,7 @@ class _Main {
     static function loadClass(className : string, callback : function(:function(:Map.<variant>) : Human) : void) : void {
         var script = dom.createElement("script") as HTMLScriptElement;
         script.addEventListener("load", function(e : Event) : void {
+            _Main.saveJSX(className);
             var eval = js.global["eval"] as function(:string) : Human;
             var factory = function(params : Map.<variant>) : Human {
                 var code = "(function() {"
@@ -41,5 +43,13 @@ class _Main {
         script.type = "text/javascript";
         script.src = className + ".js";
         dom.document.body.appendChild(script);
+    }
+
+    static function saveJSX(className : string) : void {
+        if (!js.global.hasOwnProperty("JSXs")) {
+            (dom.window as __noconvert__ Map.<Map.<variant>>)["JSXs"] = {} : Map.<variant>;
+        }
+        var JSXs = js.global["JSXs"] as Map.<variant>;
+        JSXs[className] = js.global["JSX"];
     }
 }
